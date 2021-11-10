aa26346e214dc938851c1876d5c70cd21383c139
ozan isgor <ozanisgor11@gmail.com>
2021-11-10 15:55:31 +0300

    update: change card design

--- client/src/components/TipList.vue
+++ client/src/components/TipList.vue
@@ -1,21 +1,29 @@
 <template>
-  <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-    <li v-for="tip in tips" :key="tip._id" class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200">
-      <router-link :to="{ name: 'SingleTip', params: { id: tip._id } }">
-        <div class="flex-1 flex flex-col p-8">
-          <h3 class="mt-6 text-gray-900 text-sm font-medium">{{ tip.title }}</h3>
-          <dl class="mt-1 flex-grow flex flex-col justify-between">
-            <dt class="sr-only">Tag</dt>
-            <dd class="text-gray-500 text-sm">{{ tip.tag }}</dd>
-          </dl>
-          <dl class="mt-1 flex-grow flex flex-col justify-between">
-            <dt class="sr-only">Creator</dt>
-            <dd class="text-gray-500 text-sm">{{ tip.creator }}</dd>
-          </dl>
-        </div>
-      </router-link>
-    </li>
-  </ul>
+  <div class="w-full flex justify-center mt-8">
+    <ul class="max-w-7xl grid grid-cols-1 gap-y-6 gap-x-12 sm:grid-cols-2 lg:grid-cols-3">
+      <li v-for="tip in tips" :key="tip._id" class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200 relative">
+        <router-link :to="{ name: 'SingleTip', params: { id: tip._id } }">
+          <div class="w-full flex items-center justify-between p-6 space-x-6">
+            <div class="flex-1 w-full">
+              <div class="flex items-center space-x-3">
+                <h3 class="text-gray-900 text-sm font-medium truncate">
+                  {{ tip.title }}
+                </h3>
+              </div>
+              <div class="flex-shrink-0 flex-wrap inline-block px-2 mr-2 py-0.5 my-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ tip.tag }}</div>
+
+              <p class="mt-1 text-gray-500 text-sm truncate">
+                {{ tip.content }}
+              </p>
+              <p class="mt-1 text-gray-500 text-sm truncate">
+                {{ tip.creator }}
+              </p>
+            </div>
+          </div>
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
-    <div class="max-w-5xl mx-auto">
-      <TipList />
-      <!-- Content goes here -->
-    </div>
-  </div>
+  <TipList />
 </template>
 
 <script>
