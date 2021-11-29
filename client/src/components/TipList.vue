a2935e7a29ece7717f9b884701bc76703d8d1bd2
hiba <hiba.mashfij@gmail.com>
2021-11-29 16:48:22 +0300

    yarn fix

--- client/src/components/TipList.vue
+++ client/src/components/TipList.vue
@@ -2,44 +2,10 @@
   <div class="bg-white">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:py-8 sm:px-6 lg:px-8 lg:py-10">
       <div class="text-center">
-        <p
-          class="
-            mt-1
-            text-2xl
-            font-extrabold
-            text-gray-900
-            sm:text-3xl sm:tracking-tight
-            lg:text-4xl
-          "
-        >
-          Tips
-        </p>
-        <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-          We have currently {{ tips.length }} tips in our list
-        </p>
+        <p class="mt-1 text-2xl font-extrabold text-gray-900 sm:text-3xl sm:tracking-tight lg:text-4xl">Tips</p>
+        <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">We have currently {{ tips.length }} tips in our list</p>
         <router-link :to="{ name: 'CreateTip' }">
-          <button
-            type="button"
-            class="
-              inline-flex
-              items-center
-              mt-10
-              px-4
-              py-2
-              border border-transparent
-              shadow-sm
-              text-base
-              font-medium
-              rounded-md
-              text-white
-              bg-indigo-600
-              hover:bg-indigo-700
-              focus:outline-none
-              focus:ring-2
-              focus:ring-offset-2
-              focus:ring-indigo-500
-            "
-          >
+          <button type="button" class="inline-flex items-center mt-10 px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Submit your tip
             <PlusCircleIcon class="ml-3 -mr-1 h-5 w-5" aria-hidden="true" />
           </button>
@@ -48,37 +14,11 @@
     </div>
   </div>
 
-  <div
-    class="
-      flex
-      justify-between
-      mx-auto
-      py-6
-      px-4
-      sm:py-8 sm:px-6
-      lg:px-8 lg:py-10
-      my-8
-      max-w-7xl
-    "
-  >
+  <div class="flex justify-between mx-auto py-6 px-4 sm:py-8 sm:px-6 lg:px-8 lg:py-10 my-8 max-w-7xl">
     <ul class="grid grid-cols-1 gap-y-6 gap-x-12 sm:grid-cols-2 lg:grid-cols-2">
-      <li
-        v-for="tip in tips"
-        :key="tip._id"
-        class="
-          col-span-1
-          bg-white
-          rounded-lg
-          shadow
-          divide-y divide-gray-200
-          relative
-          border-t-2 border-indigo-600
-        "
-      >
+      <li v-for="tip in tips" :key="tip._id" class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200 relative border-t-2 border-indigo-600">
         <router-link :to="{ name: 'SingleTip', params: { id: tip._id } }">
-          <div
-            class="w-full flex items-center justify-between p-6 space-x-6 h-48"
-          >
+          <div class="w-full flex items-center justify-between p-6 space-x-6 h-48">
             <div class="flex-1 w-full space-y-2">
               <div class="flex justify-between items-center space-x-3">
                 <h3 class="text-gray-900 text-sm font-medium truncate">
@@ -106,22 +46,7 @@
                 </p>
 
                 <div>
-                  <div
-                    v-for="tag in tip.tags"
-                    :key="tag"
-                    class="
-                      flex-shrink-0 flex-wrap
-                      inline-block
-                      px-2
-                      mr-2
-                      py-0.5
-                      my-1
-                      text-indigo-800 text-xs
-                      font-medium
-                      bg-indigo-100
-                      rounded-full
-                    "
-                  >
+                  <div v-for="tag in tip.tags" :key="tag" class="flex-shrink-0 flex-wrap inline-block px-2 mr-2 py-0.5 my-1 text-indigo-800 text-xs font-medium bg-indigo-100 rounded-full">
                     {{ tag }}
                   </div>
                 </div>
@@ -133,43 +58,14 @@
     </ul>
     <div class="flex flex-col">
       <div class="flex flex-col px-10 h-full mb-6">
-        <a
-          class="
-            text-gray-500
-            dark:text-gray-300
-            hover:bg-gray-400 hover:bg-opacity-10
-            font-medium
-            w-16
-            md:w-60
-            rounded-l-full
-            lg:rounded-full
-            flex
-          "
-          target="_blank"
-          rel="noreferrer"
-          href="https://stats.gallery/"
+        <a class="text-gray-500 dark:text-gray-300 hover:bg-gray-400 hover:bg-opacity-10 font-medium w-16 md:w-60 rounded-l-full lg:rounded-full flex" target="_blank" rel="noreferrer" href="https://stats.gallery/"
           ><div class="rounded-l-full py-3 pl-4 pr-3 flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-6 h-6"
-              viewBox="0 0 67.526 76.5703"
-              fill="currentColor"
-            >
-              <path
-                class="cls-1"
-                d="M37.8925,23.1852C30.964,37.3934,24.2679,51.1273,17.5691,64.86c-1.4565,2.9859-3.015,5.9266-4.3443,8.9678a2.7152,2.7152,0,0,1-3.04,1.845C7.2821,75.56,4.3717,75.64,1.4778,75.64c-.23-1.0363.3866-1.7017.7391-2.4179Q19.5456,38.0088,36.9191,2.8177c.3529-.7151.44-1.8821,1.3478-1.9241.9712-.0449,1.0345,1.1519,1.38,1.8516Q52.8455,29.4379,66.028,56.1384c.8479,1.71,1.1321,2.6369-1.3688,2.4124-2.801-.2514-6.1589.9952-8.287-.4546-2.0074-1.3676-2.7858-4.5738-4.0439-7.0028Q45.9092,38.7012,39.5325,26.2871C39.0838,25.4171,38.6179,24.5559,37.8925,23.1852Z"
-              />
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 67.526 76.5703" fill="currentColor">
+              <path class="cls-1" d="M37.8925,23.1852C30.964,37.3934,24.2679,51.1273,17.5691,64.86c-1.4565,2.9859-3.015,5.9266-4.3443,8.9678a2.7152,2.7152,0,0,1-3.04,1.845C7.2821,75.56,4.3717,75.64,1.4778,75.64c-.23-1.0363.3866-1.7017.7391-2.4179Q19.5456,38.0088,36.9191,2.8177c.3529-.7151.44-1.8821,1.3478-1.9241.9712-.0449,1.0345,1.1519,1.38,1.8516Q52.8455,29.4379,66.028,56.1384c.8479,1.71,1.1321,2.6369-1.3688,2.4124-2.801-.2514-6.1589.9952-8.287-.4546-2.0074-1.3676-2.7858-4.5738-4.0439-7.0028Q45.9092,38.7012,39.5325,26.2871C39.0838,25.4171,38.6179,24.5559,37.8925,23.1852Z" />
             </svg>
           </div>
-          <div
-            class="hidden md:flex flex-grow rounded-r-full py-3 items-center"
-          >
-            Stats Gallery<svg
-              class="w-3 text-gray-400 ml-2"
-              viewBox="0 0 14 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+          <div class="hidden md:flex flex-grow rounded-r-full py-3 items-center">
+            Stats Gallery<svg class="w-3 text-gray-400 ml-2" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 fill-rule="evenodd"
                 clip-rule="evenodd"
@@ -178,43 +74,15 @@
               ></path>
             </svg></div
         ></a>
-        <a
-          class="
-            text-gray-500
-            dark:text-gray-300
-            hover:bg-gray-400 hover:bg-opacity-10
-            font-medium
-            w-16
-            md:w-60
-            rounded-l-full
-            lg:rounded-full
-            flex
-          "
-          target="_blank"
-          rel="noreferrer"
-          href="https://near.university/"
+        <a class="text-gray-500 dark:text-gray-300 hover:bg-gray-400 hover:bg-opacity-10 font-medium w-16 md:w-60 rounded-l-full lg:rounded-full flex" target="_blank" rel="noreferrer" href="https://near.university/"
           ><div class="rounded-l-full py-3 pl-4 pr-3 flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"
-              ></path>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
+              <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
             </svg>
           </div>
-          <div
-            class="hidden md:flex flex-grow rounded-r-full py-3 items-center"
-          >
+          <div class="hidden md:flex flex-grow rounded-r-full py-3 items-center">
             Learn NEAR
-            <svg
-              class="w-3 text-gray-400 ml-2"
-              viewBox="0 0 14 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+            <svg class="w-3 text-gray-400 ml-2" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 fill-rule="evenodd"
                 clip-rule="evenodd"
@@ -223,45 +91,15 @@
               ></path>
             </svg></div
         ></a>
-        <a
-          class="
-            text-gray-500
-            dark:text-gray-300
-            hover:bg-gray-400 hover:bg-opacity-10
-            font-medium
-            w-16
-            md:w-60
-            rounded-l-full
-            lg:rounded-full
-            flex
-          "
-          target="_blank"
-          rel="noreferrer"
-          href="https://near-in-minutes.com/"
+        <a class="text-gray-500 dark:text-gray-300 hover:bg-gray-400 hover:bg-opacity-10 font-medium w-16 md:w-60 rounded-l-full lg:rounded-full flex" target="_blank" rel="noreferrer" href="https://near-in-minutes.com/"
           ><div class="rounded-l-full py-3 pl-4 pr-3 flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                clip-rule="evenodd"
-              ></path>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path>
             </svg>
           </div>
-          <div
-            class="hidden md:flex flex-grow rounded-r-full py-3 items-center"
-          >
+          <div class="hidden md:flex flex-grow rounded-r-full py-3 items-center">
             NEAR in Minutes
-            <svg
-              class="w-3 text-gray-400 ml-2"
-              viewBox="0 0 14 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+            <svg class="w-3 text-gray-400 ml-2" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 fill-rule="evenodd"
                 clip-rule="evenodd"
@@ -273,43 +111,12 @@
       </div>
 
       <hr class="w-72 dark:border-gray-700 hidden md:block mx-12" />
-      <div
-        class="
-          flex flex-col
-          p-10
-          mx-12
-          rounded-lg
-          border
-          shadow
-          border-gray-200
-          h-full
-          mt-10
-        "
-      >
-        <h1
-          class="
-            w-48
-            mt-1
-            text-2xl
-            font-extrabold
-            text-gray-900
-            sm:text-3xl sm:tracking-tight
-            lg:text-2xl
-          "
-        >
-          Popular tips
-        </h1>
+      <div class="flex flex-col p-10 mx-12 rounded-lg border shadow border-gray-200 h-full mt-10">
+        <h1 class="w-48 mt-1 text-2xl font-extrabold text-gray-900 sm:text-3xl sm:tracking-tight lg:text-2xl">Popular tips</h1>
         <ol class="list-decimal mt-6">
           <li v-for="tip in tips.slice(0, 5)" :key="tip._id" class="popular">
             <router-link :to="{ name: 'SingleTip', params: { id: tip._id } }">
-              <h3
-                class="
-                  text-gray-900 text-sm
-                  font-medium
-                  truncate
-                  hover:text-indigo-600
-                "
-              >
+              <h3 class="text-gray-900 text-sm font-medium truncate hover:text-indigo-600">
                 {{ tip.title }}
               </h3>
             </router-link>
@@ -322,17 +129,17 @@
 
 <script>
 import {
-  PlusCircleIcon,
+  PlusCircleIcon
   // TrashIcon, PencilIcon
-} from "@heroicons/vue/solid";
+} from '@heroicons/vue/solid';
 
-import { ref } from "@vue/reactivity";
-import { useRouter } from "vue-router";
-import { onMounted } from "vue";
+import { ref } from '@vue/reactivity';
+import { useRouter } from 'vue-router';
+import { onMounted } from 'vue';
 
 export default {
   components: {
-    PlusCircleIcon,
+    PlusCircleIcon
     // TrashIcon,
     // PencilIcon
   },
@@ -340,7 +147,7 @@ export default {
     const tips = ref([]);
     const router = useRouter();
 
-    const API_URL = "http://localhost:5000/tips";
+    const API_URL = 'http://localhost:5000/tips';
 
     onMounted(() => {
       getTips();
@@ -354,29 +161,29 @@ export default {
 
     async function removeTip(_id) {
       await fetch(`${API_URL}/${_id}`, {
-        method: "DELETE",
+        method: 'DELETE'
       });
       router.push({
-        name: "Home",
+        name: 'Home'
       });
       getTips();
     }
 
     async function editTip(_id) {
       router.push({
-        name: "Update",
+        name: 'Update',
         params: {
-          id: _id,
-        },
+          id: _id
+        }
       });
     }
 
     return {
       tips,
       removeTip,
-      editTip,
+      editTip
     };
-  },
+  }
 };
 </script>
 
